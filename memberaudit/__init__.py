8f736b9f125cc77ca2c8ef669634d2dde3f710a8
ErikKalkoken <erik.kalkoken@gmail.com>
2023-07-11 14:22:23 +0200

    Version bump

--- memberaudit/__init__.py
+++ memberaudit/__init__.py
@@ -4,5 +4,5 @@ and related reports for auditing, vetting and monitoring.
 
 default_app_config = "memberaudit.apps.MemberAuditConfig"
 
-__version__ = "2.9.4"
+__version__ = "2.10.0"
 __title__ = "Member Audit"
