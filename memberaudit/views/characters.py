de89936a32a454675fca6d37b2746b84127c30dd
ErikKalkoken <erik.kalkoken@gmail.com>
2023-09-14 21:31:16 +0200

    Update newly added character forced

--- memberaudit/views/characters.py
+++ memberaudit/views/characters.py
@@ -112,7 +112,7 @@ def add_character(request, token) -> HttpResponse:
             eve_character=eve_character, defaults={"is_disabled": False}
         )
     tasks.update_character.apply_async(
-        kwargs={"character_pk": character.pk},
+        kwargs={"character_pk": character.pk, "force_update": True},
         priority=MEMBERAUDIT_TASKS_NORMAL_PRIORITY,
     )
     messages.success(
