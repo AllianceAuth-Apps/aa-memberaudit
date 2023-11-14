0aa09e8ee1737fb91b0b3cb27a0d769e827b25a4
ErikKalkoken <erik.kalkoken@gmail.com>
2023-11-14 19:53:30 +0100

    Disconnect signals by default when creating test objects to make it faster

--- memberaudit/tests/utils.py
+++ memberaudit/tests/utils.py
@@ -24,40 +24,60 @@ logger = logging.getLogger(__name__)
 
 
 def create_user_from_evecharacter_with_access(
-    character_id: int,
+    character_id: int, disconnect_signals: bool = True
 ) -> Tuple[User, CharacterOwnership]:
     """Create user with access from an existing eve character and use it as main."""
     auth_character = EveCharacter.objects.get(character_id=character_id)
-    user = AuthUtils.create_user(auth_character.character_name)
-    user = AuthUtils.add_permission_to_user_by_name("memberaudit.basic_access", user)
+    user = AuthUtils.create_user(
+        auth_character.character_name, disconnect_signals=disconnect_signals
+    )
+    user = AuthUtils.add_permission_to_user_by_name(
+        "memberaudit.basic_access", user, disconnect_signals=disconnect_signals
+    )
     character_ownership = add_character_to_user(
-        user, auth_character, is_main=True, scopes=Character.get_esi_scopes()
+        user,
+        auth_character,
+        is_main=True,
+        scopes=Character.get_esi_scopes(),
+        disconnect_signals=disconnect_signals,
     )
     return user, character_ownership
 
 
-def create_memberaudit_character(character_id: int, **kwargs) -> Character:
+def create_memberaudit_character(
+    character_id: int, disconnect_signals: bool = True, **kwargs
+) -> Character:
     """Create a memberaudit character from an existing auth character
     incl. user and making it the main.
     """
-    _, character_ownership = create_user_from_evecharacter_with_access(character_id)
+    _, character_ownership = create_user_from_evecharacter_with_access(
+        character_id, disconnect_signals=disconnect_signals
+    )
     return create_character(eve_character=character_ownership.character, **kwargs)
 
 
 def add_auth_character_to_user(
-    user: User, character_id: int, scopes=None
+    user: User, character_id: int, scopes=None, disconnect_signals=True
 ) -> CharacterOwnership:
     auth_character = EveCharacter.objects.get(character_id=character_id)
     if not scopes:
         scopes = Character.get_esi_scopes()
 
-    return add_character_to_user(user, auth_character, is_main=False, scopes=scopes)
+    return add_character_to_user(
+        user,
+        auth_character,
+        is_main=False,
+        scopes=scopes,
+        disconnect_signals=disconnect_signals,
+    )
 
 
 def add_memberaudit_character_to_user(
-    user: User, character_id: int, **kwargs
+    user: User, character_id: int, disconnect_signals: bool = True, **kwargs
 ) -> Character:
-    character_ownership = add_auth_character_to_user(user, character_id)
+    character_ownership = add_auth_character_to_user(
+        user, character_id, disconnect_signals=disconnect_signals
+    )
     return create_character(eve_character=character_ownership.character, **kwargs)
 
 
