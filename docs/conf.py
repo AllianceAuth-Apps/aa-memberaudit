5c998aedf6dc7aefb1f23692974d47c36a19527f
ErikKalkoken <erik.kalkoken@gmail.com>
2023-12-15 16:30:32 +0100

    Attempt to fix docs build

--- docs/conf.py
+++ docs/conf.py
@@ -49,7 +49,7 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-autodoc_mock_imports = ["allianceauth"]
+autodoc_mock_imports = ["allianceauth.authentication.task_statistics.signals"]
 
 # -- Options for HTML output -------------------------------------------------
 
