6dae6cd18665567488f2ad19e1c40d5cbe05fdbf
ErikKalkoken <erik.kalkoken@gmail.com>
2025-02-10 16:47:26 +0100

    Version bump and docu

--- memberaudit/__init__.py
+++ memberaudit/__init__.py
@@ -5,5 +5,5 @@ and related reports for auditing, vetting and monitoring.
 # pylint: disable=invalid-name
 default_app_config = "memberaudit.apps.MemberAuditConfig"
 
-__version__ = "3.12.2"
+__version__ = "3.13.0"
 __title__ = "Member Audit"
