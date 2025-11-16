4583874d607025f25b3fe99fd2882e721c6ec564
ErikKalkoken <erik.kalkoken@gmail.com>
2025-11-16 15:36:19 +0100

    feat: remove unused when_esi_is_offline()

--- memberaudit/tests/test_decorators.py
+++ memberaudit/tests/test_decorators.py
@@ -1,19 +1,12 @@
-from unittest.mock import patch
-
 from django.http import HttpResponse
 from django.test import RequestFactory, TestCase
 from esi.errors import TokenError
 from esi.models import Token
 
 from allianceauth.tests.auth_utils import AuthUtils
-from app_utils.esi import EsiDailyDowntime, EsiOffline, EsiStatus
 from app_utils.testing import NoSocketsTestCase, generate_invalid_pk
 
-from memberaudit.decorators import (
-    fetch_character_if_allowed,
-    fetch_token_for_character,
-    when_esi_is_available,
-)
+from memberaudit.decorators import fetch_character_if_allowed, fetch_token_for_character
 from memberaudit.models import Character
 
 from .testdata.factories import create_character_from_user
@@ -127,70 +120,3 @@ class TestFetchToken(TestCase):
 
         with self.assertRaises(TokenError):
             dummy(self, self.character)
-
-
-@when_esi_is_available
-def when_esi_is_available_func():
-    return "ok"
-
-
-@patch(MODULE_PATH + ".IS_TESTING", False)
-@patch(MODULE_PATH + ".cache.set", spec=True)
-@patch(MODULE_PATH + ".cache.get", spec=True)
-@patch(MODULE_PATH + ".fetch_esi_status")
-class TestWhenEsiIsAvailable(NoSocketsTestCase):
-    def test_should_run_task_when_esi_is_available(
-        self, mock_fetch_esi_status, mock_cache_get, mock_cache_set
-    ):
-        # given
-        mock_cache_get.return_value = None
-        mock_fetch_esi_status.return_value = EsiStatus(True, 99, 60)
-        # when/then
-        result = when_esi_is_available_func()
-        # then
-        self.assertEqual(result, "ok")
-
-    def test_should_raise_error_when_esi_is_offline(
-        self, mock_fetch_esi_status, mock_cache_get, mock_cache_set
-    ):
-        # given
-        mock_cache_get.return_value = None
-        mock_fetch_esi_status.return_value = EsiStatus(False, 99, 60)
-        # when/then
-        with self.assertRaises(EsiOffline):
-            when_esi_is_available_func()
-
-    def test_should_complete_task_early_when_downtime(
-        self, mock_fetch_esi_status, mock_cache_get, mock_cache_set
-    ):
-        # given
-        mock_cache_get.return_value = None
-        mock_fetch_esi_status.side_effect = EsiDailyDowntime
-        # when/then
-        result = when_esi_is_available_func()
-        # then
-        self.assertIsNone(result)
-
-    def test_should_take_status_from_cache_when_availale(
-        self, mock_fetch_esi_status, mock_cache_get, mock_cache_set
-    ):
-        # given
-        mock_cache_get.return_value = EsiStatus(True, 99, 60)
-        mock_fetch_esi_status.side_effect = RuntimeError
-        # when/then
-        result = when_esi_is_available_func()
-        # then
-        self.assertEqual(result, "ok")
-        self.assertFalse(mock_cache_set.called)
-
-    def test_should_run_task_when_esi_is_available_and_store_in_cache(
-        self, mock_fetch_esi_status, mock_cache_get, mock_cache_set
-    ):
-        # given
-        mock_cache_get.return_value = None
-        mock_fetch_esi_status.return_value = EsiStatus(True, 99, 60)
-        # when/then
-        result = when_esi_is_available_func()
-        # then
-        self.assertEqual(result, "ok")
-        self.assertTrue(mock_cache_set.called)
