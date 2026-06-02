a2d25cb313f3fa44a62abd71b37a3d65f6ac1d27
ErikKalkoken <erik.kalkoken@gmail.com>
2026-06-02 20:32:54 +0200

    chore: update esi specs

--- memberaudit/__init__.py
+++ memberaudit/__init__.py
@@ -5,5 +5,5 @@ and related reports for auditing, vetting and monitoring.
 # pylint: disable=invalid-name
 default_app_config = "memberaudit.apps.MemberAuditConfig"
 
-__version__ = "5.0.0"
+__version__ = "5.0.1"
 __title__ = "Member Audit"
