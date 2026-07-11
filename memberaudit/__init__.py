ef260c17eaf87e314860a07f94f13779ddf3e0a8
ErikKalkoken <erik.kalkoken@gmail.com>
2026-07-11 22:52:36 +0200

    feat: update links for github

--- memberaudit/__init__.py
+++ memberaudit/__init__.py
@@ -5,5 +5,5 @@ and related reports for auditing, vetting and monitoring.
 # pylint: disable=invalid-name
 default_app_config = "memberaudit.apps.MemberAuditConfig"
 
-__version__ = "5.0.2"
+__version__ = "5.0.3"
 __title__ = "Member Audit"
