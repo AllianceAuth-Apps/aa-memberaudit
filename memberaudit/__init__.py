d5b0ec0d9e47dbca0c0eefc5b8ca8320ea720b3d
ErikKalkoken <erik.kalkoken@gmail.com>
2026-07-29 20:44:29 +0200

    fix: license in pyproject toml

--- memberaudit/__init__.py
+++ memberaudit/__init__.py
@@ -5,5 +5,5 @@ and related reports for auditing, vetting and monitoring.
 # pylint: disable=invalid-name
 default_app_config = "memberaudit.apps.MemberAuditConfig"
 
-__version__ = "5.0.4"
+__version__ = "5.1.0"
 __title__ = "Member Audit"
